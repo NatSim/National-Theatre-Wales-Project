600fddb6659b9cc3ffac8d211d9e61d76dcec738
NatSim <natsimbig@gmail.com>
2021-04-01 12:40:49 +0100

    Centering SA page layout text ,creating equal height and width columns

--- client/src/Pages/Act/SimpleActsPage.js
+++ client/src/Pages/Act/SimpleActsPage.js
@@ -62,24 +62,31 @@ const SimpleActsPage = (props) => {
       {/* Background END HERE */}
 
       {/*SIMPLE ACTS INTRO TEXT-START HERE */}
-      <div className="text-video-control">
-        <article className="challenge-text">
-          With a few simple acts we can change how we interact with our
-          environment, and how we interact with each other.
-          <br />
-          At the heart of Go Tell the Bees is the hope that by doing we can
-          learn from each other, we can build confidence and skills and that we
-          can experience through all of the senses.
-        </article>
-        <article className="challenge-text">
-          We want this production to have a positive impact on communities.
-          Learning in an active and imaginative way is critical to this.
-          <br />
-          NTW TEAM have created and curated a hive of information, knowledge,
-          activity and inspiration that people of all ages can use as a
-          springboard for learning. Explore each Simple Act through activities,
-          ideas and educational resources:
-        </article>
+      <div className="text-container">
+        <div className="text-video-row">
+          <div className="text-video-column">
+            <article className="challenge-text">
+              With a few simple acts we can change how we interact with our
+              environment, and how we interact with each other.
+              <br />
+              <br />
+              At the heart of Go Tell the Bees is the hope that by doing we can
+              learn from each other, we can build confidence and skills and that
+              we can experience through all of the senses.
+            </article>
+          </div>
+          <div className="text-video-column">
+            <article className="challenge-text">
+              We want this production to have a positive impact on communities.
+              Learning in an active and imaginative way is critical to this.
+              <br />
+              NTW TEAM have created and curated a hive of information,
+              knowledge, activity and inspiration that people of all ages can
+              use as a springboard for learning. Explore each Simple Act through
+              activities, ideas and educational resources:
+            </article>
+          </div>
+        </div>
       </div>
       {/*SIMPLE ACTS INTRO TEXT-END HERE */}
 
--- client/src/components/PageContent/Challenges/ChallengeChoicePage.js
+++ client/src/components/PageContent/Challenges/ChallengeChoicePage.js
@@ -17,23 +17,14 @@ const ChallengeItem = (props) => {
 
   return (
     <>
+      {/* Challenge top background START HERE */}
       <div className="challenge-page">
         <Header />
         <SideNav />
-
         <section className="prompt-act-container">
           <div className="prompt-text-container">
             <h1 className="prompt-text">{props.direction}</h1>
-          </div>
-          <div className="act-control-container">
             <h2 className="act-control-child">{props.act}</h2>
-          </div>
-          <div className="image-container">
-            <img
-              className="image"
-              src={props.image1}
-              alt={"simple act challenge"}
-            />
           </div>
 
           <div className="intro-text">
@@ -41,62 +32,71 @@ const ChallengeItem = (props) => {
           </div>
         </section>
 
-        <div className="challenge-page-background2">
-          <section className="article-image-container">
-            <div className="flex-parent">
-              <div className="flex-child">
-                <h3 className="text2-subheading">{props.text2Subheading}</h3>
-                <p className="text-control"> {props.text2}</p>
-              </div>
-              <div className="flex-child-image">
-                <img
-                  className="place-holder"
-                  src={props.image2}
-                  alt={"placeholder"}
-                />
-              </div>
-            </div>
-            <div className="flex-child">
-              <h3 className="discover-subheading">{props.discoverHeading}</h3>
-              <span>
-                {props.urlFilm} {props.urlBook} {props.urlArtist}
-              </span>
-              <div className="flex-child">
-                <h3 className="resources-subheading">
-                  {props.resourcesSubheading}
-                </h3>
-                <div className="flex-child">
-                  <h3 className="resources-subheading">
-                    {props.charityHeading}
-                  </h3>
-                  <span>{props.urlCharity}</span>
-                </div>
-                <ul>
-                  <li className="nav-link-control">
-                    {props.urlResources} (Under 16s)
-                  </li>
-                  <li className="nav-link-control">
-                    {props.urlResources2} (Under 16s)
-                  </li>
-                  <li className="nav-link-control">
-                    {props.urlResources3}{" "}
-                    {props.urlResources3 ? "(Under 16s)" : ""}
-                  </li>
-                </ul>
-              </div>
-            </div>
-            <div className="flex-child" id="border-control">
-              <h3 className="quote-heading">{props.quoteHeading}</h3>
-              <p>{props.quote}</p>
-            </div>
-          </section>
-          <CompleteChallengeButton onClickHandler={onClickHandler} />
-
-          <div className="welldone-message">
-            <p>{welldone}</p>
-          </div>
+        <div className="image-container">
+          <img
+            className="image"
+            src={props.image1}
+            alt={"simple act challenge"}
+          />
         </div>
       </div>
+      {/* Challenge top background END HERE */}
+      {/* Challenge section START HERE */}
+      <div className="challenge-page-background2">
+        <section className="article-image-container">
+          <div className="flex-parent">
+            <div className="flex-child">
+              <h3 className="text2-subheading">{props.text2Subheading}</h3>
+              <p className="text-control"> {props.text2}</p>
+            </div>
+            <div className="flex-child-image">
+              <img
+                className="place-holder"
+                src={props.image2}
+                alt={"placeholder"}
+              />
+            </div>
+          </div>
+          <div className="flex-child">
+            <h3 className="discover-subheading">{props.discoverHeading}</h3>
+            <span>
+              {props.urlFilm} {props.urlBook} {props.urlArtist}
+            </span>
+            <div className="flex-child">
+              <h3 className="resources-subheading">
+                {props.resourcesSubheading}
+              </h3>
+              <div className="flex-child">
+                <h3 className="resources-subheading">{props.charityHeading}</h3>
+                <span>{props.urlCharity}</span>
+              </div>
+              <ul>
+                <li className="nav-link-control">
+                  {props.urlResources} (Under 16s)
+                </li>
+                <li className="nav-link-control">
+                  {props.urlResources2} (Under 16s)
+                </li>
+                <li className="nav-link-control">
+                  {props.urlResources3}{" "}
+                  {props.urlResources3 ? "(Under 16s)" : ""}
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex-child" id="border-control">
+            <h3 className="quote-heading">{props.quoteHeading}</h3>
+            <p>{props.quote}</p>
+          </div>
+        </section>
+        <CompleteChallengeButton onClickHandler={onClickHandler} />
+
+        <div className="welldone-message">
+          <p>{welldone}</p>
+        </div>
+      </div>
+
+      {/* Challenge section END HERE */}
     </>
   );
 };
