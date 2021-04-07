740ea884dcb931666fedb005ce6f90eab8ce8226
NatSim <natsimbig@gmail.com>
2021-04-07 21:43:04 +0100

    changing routes on feature cards to home for welsh history push, changing adjustments to card min-height to be added

--- client/src/components/PageContent/Features/Features.js
+++ client/src/components/PageContent/Features/Features.js
@@ -1,9 +1,17 @@
 import React from "react";
+import { useHistory } from "react-router";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import "./Features.css";
 
 const Features = (props) => {
+  const history = useHistory();
+  //On Button click redirects user to home page
+
+  const navigateToAboutPages = () => {
+    history.push(props.links);
+  };
+
   return (
     <>
       <br />
@@ -12,7 +20,7 @@ const Features = (props) => {
         <Card.Body>
           <Card.Title>{props.title}</Card.Title>
           <Card.Text>{props.text}</Card.Text>
-          <Button variant="primary" href={props.links}>
+          <Button variant="primary" onClick={navigateToAboutPages}>
             {props.button}
           </Button>
         </Card.Body>
