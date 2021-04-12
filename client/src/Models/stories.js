5bde7a77396ab6d44a26a7c088f160eb6a6d9134
NatSim <natsimbig@gmail.com>
2021-04-12 11:04:27 +0100

    Adding welsh translation to adult stories connect page

--- client/src/Models/stories.js
+++ client/src/Models/stories.js
@@ -1,14 +1,14 @@
 const stories = [
   {
     id_: 1,
-    videoTitle: "Plant a seed – Outside",
-    videoTitle: "Take a walk – Peak",
-    videoTitle: "Connect with someone new – Mermaid",
+    videoTitle1: "Plant a seed – Outside",
+    videoTitle2: "Take a walk – Peak",
+    videoTitle3: "Connect with someone new – Mermaid",
 
-    videoTitle: "Consume a little less - Forage",
-    videoTitle: "Be kind to your neighbour - Drift",
-    videoTitle: "Consider the stars - Across the Universe",
-    videoTitle: "Learn one new thing about nature - Den",
+    videoTitle4: "Consume a little less - Forage",
+    videoTitle5: "Be kind to your neighbour - Drift",
+    videoTitle6: "Consider the stars - Across the Universe",
+    videoTitle7: "Learn one new thing about nature - Den",
     videoMono1: "https://www.youtube.com/watch?v=xV2FF-YUy94",
     videoMono2: "https://www.youtube.com/watch?v=xV2FF-YUy94",
   },
--- client/src/Pages/Connect/StoriesPage.js
+++ client/src/Pages/Connect/StoriesPage.js
@@ -37,114 +37,241 @@ const StoriesPageA = (props) => {
             </h3>
 
             <p className="story-blurbs">
-              {t("storiesContent.4")}{" "}
-              <em className="blue-font">{t("storiesContent.5")}</em>
+              {props.id_ === 1 ? (
+                <>
+                  <p>
+                    {t("storiesContent.4")}
+                    <em className="blue-font">{t("storiesContent.5")}</em>
+                  </p>
+                </>
+              ) : (
+                <Trans i18nKey="storiesContent.7"></Trans>
+              )}
             </p>
           </section>
           <section className="video-container-parent">
             <div className="video-text-control">
-              <div className="video-container-child">
-                <VideoPlayer
-                  url={t("storyHref.0")}
-                  width={"246px"}
-                  height={"150px"}
-                />
-              </div>
-              <div className="story-info-container">
-                <h4>{t("story_Title.0")}</h4>
-                <article>{t("storyBlurb.0")}</article>
-              </div>
+              {props.id_ === 1 ? (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.0")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              ) : (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.7")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              )}
+              {props.id_ === 1 ? (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.0")}</h4>
+                  <article>{t("storyBlurb.0")}</article>
+                </div>
+              ) : (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.7")}</h4>
+                  <article>{t("storyBlurb.7")}</article>
+                </div>
+              )}
             </div>
             {/* Story 1 container END HERE */}
             {/* Story 2 container START HERE */}
             <div className="video-text-control">
-              <div className="video-container-child">
-                <VideoPlayer
-                  url={t("storyHref.1")}
-                  width={"246px"}
-                  height={"150px"}
-                />
-              </div>
-              <div className="story-info-container">
-                <h4>{t("story_Title.1")}</h4>
-                <article>{t("storyBlurb.1")}</article>
-              </div>
+              {props.id_ === 1 ? (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.1")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              ) : (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.7")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              )}
+              {props.id_ === 1 ? (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.1")}</h4>
+                  <article>{t("storyBlurb.1")}</article>
+                </div>
+              ) : (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.8")}</h4>
+                  <article>{t("storyBlurb.8")}</article>
+                </div>
+              )}
             </div>
 
             {/* Story 2 container END HERE */}
             {/* Story 3 container START HERE */}
             <div className="video-text-control">
-              <div className="video-container-child">
-                <VideoPlayer
-                  url={t("storyHref.2")}
-                  width={"246px"}
-                  height={"150px"}
-                />
-              </div>
-              <div className="story-info-container">
-                <h4>{t("story_Title.2")}</h4>
-                <article>{t("storyBlurb.2")}</article>
-              </div>
+              {props.id_ === 1 ? (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.2")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              ) : (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.7")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              )}
+              {props.id_ === 1 ? (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.2")}</h4>
+                  <article>{t("storyBlurb.2")}</article>
+                </div>
+              ) : (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.9")}</h4>
+                  <article>{t("storyBlurb.9")}</article>
+                </div>
+              )}
             </div>
             {/* Story 3 container END HERE */}
             {/* Story 4 container START HERE */}
             <div className="video-text-control">
-              <div className="video-container-child">
-                <VideoPlayer
-                  url={t("storyHref.3")}
-                  width={"246px"}
-                  height={"150px"}
-                />
-              </div>
-              <div className="story-info-container">
-                <h4>{t("story_Title.3")}</h4>
-                <article>{t("storyBlurb.3")}</article>
-              </div>
+              {props.id_ === 1 ? (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.3")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              ) : (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.7")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              )}
+              {props.id_ === 1 ? (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.3")}</h4>
+                  <article>{t("storyBlurb.3")}</article>
+                </div>
+              ) : (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.10")}</h4>
+                  <article>{t("storyBlurb.10")}</article>
+                </div>
+              )}
             </div>
             {/* Story 4 container END HERE */}
             {/* Story 5 container START HERE */}
             <div className="video-text-control">
-              <div className="video-container-child">
-                <VideoPlayer
-                  url={t("storyHref.4")}
-                  width={"246px"}
-                  height={"150px"}
-                />
-              </div>
-              <div className="story-info-container">
-                <h4>{t("story_Title.4")}</h4>
-                <article>{t("storyBlurb.4")}</article>
-              </div>
+              {props.id_ === 1 ? (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.4")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              ) : (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.7")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              )}
+              {props.id_ === 1 ? (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.4")}</h4>
+                  <article>{t("storyBlurb.4")}</article>
+                </div>
+              ) : (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.11")}</h4>
+                  <article>{t("storyBlurb.11")}</article>
+                </div>
+              )}
             </div>
             {/* Story 5 container END HERE */}
             {/* Story 6 container START HERE */}
             <div className="video-text-control">
-              <div className="video-container-child">
-                <VideoPlayer
-                  url={t("storyHref.5")}
-                  width={"246px"}
-                  height={"150px"}
-                />
-              </div>
-              <div className="story-info-container">
-                <h4>{t("story_Title.5")}</h4>
-                <article>{t("storyBlurb.5")}</article>
-              </div>
+              {props.id_ === 1 ? (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.5")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              ) : (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.7")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              )}
+              {props.id_ === 1 ? (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.5")}</h4>
+                  <article>{t("storyBlurb.5")}</article>
+                </div>
+              ) : (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.12")}</h4>
+                  <article>{t("storyBlurb.12")}</article>
+                </div>
+              )}
             </div>
             {/* Story 6 container END HERE */}
             {/* Story 7 container START HERE */}
             <div className="video-text-control">
-              <div className="video-container-child">
-                <VideoPlayer
-                  url={t("storyHref.6")}
-                  width={"246px"}
-                  height={"150px"}
-                />
-              </div>
-              <div className="story-info-container">
-                <h4>{t("story_Title.6")}</h4>
-                <article>{t("storyBlurb.")}</article>
-              </div>
+              {props.id_ === 1 ? (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.6")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              ) : (
+                <div className="video-container-child">
+                  <VideoPlayer
+                    url={t("storyHref.7")}
+                    width={"246px"}
+                    height={"150px"}
+                  />
+                </div>
+              )}
+              {props.id_ === 1 ? (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.6")}</h4>
+                  <article>{t("storyBlurb.6")}</article>
+                </div>
+              ) : (
+                <div className="story-info-container">
+                  <h4>{t("story_Title.13")}</h4>
+                  <article>{t("storyBlurb.13")}</article>
+                </div>
+              )}
             </div>
             {/* Story 7 container END HERE */}
           </section>
