c73c5cf2f3fab8f7e6294a65520100c00cc9ee07
NatSim <natsimbig@gmail.com>
2021-04-20 16:17:45 +0100

    build attempt deploy

--- client/src/Pages/Listen/MusicPage.js
+++ client/src/Pages/Listen/MusicPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Trans, useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 import Header from "../../components/Header/Header";
 import VideoPlayer from "../../components/PageContent/VideoPlayer/VideoPlayer";
 import "./MusicPage.css";
