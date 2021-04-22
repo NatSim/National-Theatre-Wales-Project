96e62e367c82e59b816792dcc6d0d0f7a143b86f
NatSim <natsimbig@gmail.com>
2021-04-22 15:04:58 +0100

    show page layout edit

--- client/src/Pages/Show/ShowPage.js
+++ client/src/Pages/Show/ShowPage.js
@@ -1,13 +1,26 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import Header from "../../components/Header/Header";
 import SideNav from "../../components/Nav/SideNav";
 import "./ShowPage.css";
 
 const ShowPage = () => {
+  const { i18n } = useTranslation();
+  const changeLanguage = (language) => {
+    i18n.changeLanguage(language);
+  };
   return (
     <>
       <SideNav />
       <Header />
+      {/* Translation Button START HERE */}
+      <div className="translate-button">
+        <div className="translate-button-control">
+          <button onClick={() => changeLanguage("en")}>En</button>
+          <button onClick={() => changeLanguage("welsh")}>Cymraeg</button>
+        </div>
+      </div>
+      {/* Translation Button END HERE */}
       <div className="live-background-container"></div>
       <h2 className="subtitle5">Show Programme</h2>
       <div>
