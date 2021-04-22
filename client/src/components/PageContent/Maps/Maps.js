609dd823b20e1d2902239633802adde94b4a1f05
NatSim <natsimbig@gmail.com>
2021-04-22 14:33:54 +0100

    Adding Map content

--- client/src/components/PageContent/Maps/Maps.js
+++ client/src/components/PageContent/Maps/Maps.js
@@ -1,7 +1,12 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import mapPlaceholder from "../../../images/Maps/Pembrok.jfif";
 import StDavidsPol from "../../../images/Maps/St_Davids_pollinator.png";
+import PemCoastPath from "../../../images/Maps/PembCoastalPath.png";
+import GetTheBoys from "../../../images/Maps/GetTheBoysLift.png";
+import LittlePantryShop from "../../../images/Maps/LittlePantryWasteShop.png";
+import Skrinkle from "../../../images/Maps/Skrinkle.png";
+import CroesoAbergwaun from "../../../images/Maps/CroesoAbergwaun.png";
+import TyCanolNatureTrai from "../../../images/Maps/TyCanolNatureTrai.png";
 import "./Maps.css";
 
 const Maps = () => {
@@ -29,114 +34,109 @@ const Maps = () => {
           </p>
         </section>
         {/* Map1 Plant a Seed END HERE */}
+        {/* Map2 Take a Walk a Seed START HERE */}
         <section className="map-container-control-spacebetween">
-          <div>
-            <ol>
-              <li>Plant a Seed</li>
-            </ol>
-
-            <span>Got to... St Davids Pollinator Trail</span>
+          <div className="text-control-maps">
+            <span>{t("mapAct.1")}</span> <br />
+            <span>{t("mapLocation.1")}</span>
           </div>
           <p>
-            <a href="https://g.page/thebugfarm?share ">
+            <a href="https://goo.gl/maps/3LMrdPYMSZThB5qGA ">
               <img
                 className="map-control"
-                src={StDavidsPol}
+                src={PemCoastPath}
                 alt="tour-locations"
               />
             </a>
           </p>
         </section>
-        <section className="map-container-control-spacebetween">
-          <div>
-            <ol>
-              <li>Plant a Seed</li>
-            </ol>
+        {/* Map2 Take a Walk END HERE */}
 
-            <span>Got to... St Davids Pollinator Trail</span>
+        {/* Map3 Connect START HERE */}
+        <section className="map-container-control-spacebetween">
+          <div className="text-control-maps">
+            <span>{t("mapAct.2")}</span> <br />
+            <span>{t("mapLocation.2")}</span>
           </div>
           <p>
-            <a href="https://g.page/thebugfarm?share ">
+            <a href="https://g.page/gettheboysalift?share  ">
               <img
                 className="map-control"
-                src={StDavidsPol}
+                src={GetTheBoys}
                 alt="tour-locations"
               />
             </a>
           </p>
         </section>
+        {/* Map3 Connect END HERE */}
+        {/* Map4 Consume Less START HERE */}
         <section className="map-container-control-spacebetween">
-          <div>
-            <ol>
-              <li>Plant a Seed</li>
-            </ol>
-
-            <span>Got to... St Davids Pollinator Trail</span>
+          <div className="text-control-maps">
+            <span>{t("mapAct.3")}</span> <br />
+            <span>{t("mapLocation.3")}</span>
           </div>
           <p>
-            <a href="https://g.page/thebugfarm?share ">
+            <a href="https://goo.gl/maps/h4oxgLR55vPCQDFm8 ">
               <img
                 className="map-control"
-                src={StDavidsPol}
+                src={LittlePantryShop}
                 alt="tour-locations"
               />
             </a>
           </p>
         </section>
+        {/* Map4 Consume END HERE */}
+        {/* Map5 Consider START HERE */}
         <section className="map-container-control-spacebetween">
-          <div>
-            <ol>
-              <li>Plant a Seed</li>
-            </ol>
-
-            <span>Got to... St Davids Pollinator Trail</span>
+          <div className="text-control-maps">
+            <span>{t("mapAct.4")}</span> <br />
+            <span>{t("mapLocation.4")}</span>
           </div>
           <p>
-            <a href="https://g.page/thebugfarm?share ">
+            <a href="https://goo.gl/maps/rwwK4BPeYtE4ta3i9 ">
               <img
                 className="map-control"
-                src={StDavidsPol}
+                src={Skrinkle}
                 alt="tour-locations"
               />
             </a>
           </p>
         </section>
+        {/* Map5 Consider Less END HERE */}
+        {/* Map6 Be Kind the stars START HERE */}
         <section className="map-container-control-spacebetween">
-          <div>
-            <ol>
-              <li>Plant a Seed</li>
-            </ol>
-
-            <span>Got to... St Davids Pollinator Trail</span>
+          <div className="text-control-maps">
+            <span>{t("mapAct.5")}</span> <br />
+            <span>{t("mapLocation.5")}</span>
           </div>
           <p>
-            <a href="https://g.page/thebugfarm?share ">
+            <a href="https://goo.gl/maps/Km3o7kqh58wqsxbj6  ">
               <img
                 className="map-control"
-                src={StDavidsPol}
+                src={CroesoAbergwaun}
                 alt="tour-locations"
               />
             </a>
           </p>
         </section>
+        {/* Map6 Be Kind the stars END HERE */}
+        {/* Map7 Learn START HERE */}
         <section className="map-container-control-spacebetween">
-          <div>
-            <ol>
-              <li>Plant a Seed</li>
-            </ol>
-
-            <span>Got to... St Davids Pollinator Trail</span>
+          <div className="text-control-maps">
+            <span>{t("mapAct.6")}</span> <br />
+            <span>{t("mapLocation.6")}</span>
           </div>
           <p>
-            <a href="https://g.page/thebugfarm?share ">
+            <a href="https://goo.gl/maps/J8rGz6HPxqHogaR36 ">
               <img
                 className="map-control"
-                src={StDavidsPol}
+                src={TyCanolNatureTrai}
                 alt="tour-locations"
               />
             </a>
           </p>
         </section>
+        {/* Map7 Learn END HERE */}
       </div>
     </>
   );
