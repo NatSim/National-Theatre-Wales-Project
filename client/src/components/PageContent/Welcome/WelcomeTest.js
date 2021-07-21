5451e7d0850c73e6fbbc9dfa495d12da5cb8bc48
NatSim <natsimbig@gmail.com>
2021-07-21 10:44:52 +0100

    removed empty state space from transition with comp operator

--- client/src/components/PageContent/Welcome/WelcomeTest.js
+++ client/src/components/PageContent/Welcome/WelcomeTest.js
@@ -2,18 +2,21 @@ import React, { useState, useEffect } from "react";
 import { Trans, useTranslation } from "react-i18next";
 
 function WelcomeTest() {
+  const { t } = useTranslation();
   const { i18n } = useTranslation();
   const changeLanguage = (language) => {
     i18n.changeLanguage(language);
   };
 
-  const sentences = [
-    <Trans i18nKey="welcomeComponent.0"></Trans>,
+  const sentences = ["hello1", "bonjour2", "hola3"];
+  {
+    /* <Trans i18nKey="welcomeComponent.0"></Trans>,
     <Trans i18nKey="welcomeStory.0"></Trans>,
-    <Trans i18nKey="welcomeQuestion.0"></Trans>,
-  ];
-
+    <Trans i18nKey="welcomeQuestion.0"></Trans>, */
+  }
   const [intro, setIntro] = useState(sentences[0]);
+  console.log(sentences.indexOf(intro));
+  console.log(sentences.length);
 
   useEffect(() => {
     //ToDo: Change name every 3 seconds here
@@ -22,10 +25,11 @@ function WelcomeTest() {
       //Change name logic goes here
 
       setIntro((previousIntro) => {
-        const newIndex = sentences.indexOf(intro) + 1;
-        if (newIndex > sentences.length) {
+        let newIndex = sentences.indexOf(intro) + 1;
+        if (newIndex === sentences.length) {
           newIndex = 0;
         }
+
         const newIntro = sentences[newIndex];
         return newIntro;
       });
@@ -34,7 +38,7 @@ function WelcomeTest() {
     return () => {
       clearInterval(timer);
     };
-  }, [intro]); // [] - means, it runs only once after mounting the component
+  }); // [] - means, it runs only once after mounting the component
 
   return (
     <>
