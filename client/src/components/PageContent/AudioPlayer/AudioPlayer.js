dbbb182fa62e88aa5586b4e3b92050a95bac1530
NatSim <natsimbig@gmail.com>
2021-08-05 21:02:43 +0100

    remove button border, remove only screen and on media queries

--- client/src/components/PageContent/AudioPlayer/AudioPlayer.js
+++ client/src/components/PageContent/AudioPlayer/AudioPlayer.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./AudioPlayer.css";
 import * as FaIcons from "react-icons/fa";
 import Dawn from "./tracks/dawn.wav";
@@ -7,15 +7,39 @@ function AudioPlayer() {
   // play/pause tracker
 
   const [isPlaying, setIsPlaying] = useState(false);
+  const [duration, setDuration] = useState(0);
+  const [currentTime, setCurrentTime] = useState(0);
 
   //references
   const audioPlayer = useRef(); //for audio component
+  const progressBar = useRef(); //ref to progress bar
 
+  useEffect(() => {
+    //calculate the duration to seconds
+    const seconds = Math.floor(audioPlayer.current.duration);
+
+    setDuration(seconds);
+    progressBar.current.max = seconds;
+  }, [audioPlayer?.current?.loadedmetadata, audioPlayer?.current?.readyState]);
+
+  const calculateTime = (secs) => {
+    const minutes = Math.floor(secs / 60);
+
+    const returnMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+
+    const seconds = Math.floor(secs % 60);
+
+    const returnSeconds = secs < 10 ? `0${seconds}` : `${seconds}`;
+
+    return `${returnMinutes} :${returnSeconds}`;
+  };
+
+  //Play/Pause function
   const togglePlayPause = () => {
     const prevValue = isPlaying;
 
     setIsPlaying(!prevValue);
-    if (prevValue) {
+    if (!prevValue) {
       audioPlayer.current.play();
       console.log("Play");
     } else {
@@ -24,9 +48,14 @@ function AudioPlayer() {
     }
   };
 
+  const changeRange = () => {
+    audioPlayer.current.currentTime = progressBar.current.value;
+    progressBar.current.style.setProperty("--seek-before-width");
+  };
+
   return (
     <div className="default-button audio-style-control">
-      <audio ref={audioPlayer} type="audio" src={Dawn} preload="none">
+      <audio ref={audioPlayer} type="audio" src={Dawn} preload="metadata">
         Your browser does not support the audio element.
       </audio>
       <section className="audio-track-title-container">
@@ -50,14 +79,19 @@ function AudioPlayer() {
       </section>
       <section className="audio-track-title-container">
         {/* current time */}
-        <div>0:00</div>
+        <div>{calculateTime(currentTime)}</div>
 
         {/* progress bar */}
         <div>
-          <input type="range" />
+          <input
+            type="range"
+            defaultValue="0"
+            ref={progressBar}
+            onChange={changeRange}
+          />
         </div>
         {/* duration */}
-        <div>Duration</div>
+        <div>{duration && !isNaN(duration) && calculateTime(duration)}</div>
         {/*CUSTOM CONTROLS START */}
       </section>
     </div>
