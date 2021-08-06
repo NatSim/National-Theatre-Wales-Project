d711df49567909332259a1b42df47adedfcb56bc
NatSim <natsimbig@gmail.com>
2021-08-06 15:38:19 +0100

    audio player adding to Music Page with translations and attributes

--- client/src/Pages/Listen/MusicPage.js
+++ client/src/Pages/Listen/MusicPage.js
@@ -5,6 +5,15 @@ import "./MusicPage.css";
 import SideNav from "../../components/Nav/SideNav";
 import AudioPlayer from "../../components/PageContent/AudioPlayer/AudioPlayer";
 import Dawn from "../../components/PageContent/AudioPlayer/tracks/dawn.wav";
+import CatchThisQuiet from "../../components/PageContent/AudioPlayer/tracks/catchthisquiet.mp3";
+import Dryw from "../../components/PageContent/AudioPlayer/tracks/dryw.wav";
+import Gwenyn from "../../components/PageContent/AudioPlayer/tracks/gwenyn.wav";
+import JustToBee from "../../components/PageContent/AudioPlayer/tracks/JustToBee.mp3";
+import Merfolk from "../../components/PageContent/AudioPlayer/tracks/merfolk.wav";
+import Petra from "../../components/PageContent/AudioPlayer/tracks/petra.wav";
+import Stars from "../../components/PageContent/AudioPlayer/tracks/stars.wav";
+import Trees from "../../components/PageContent/AudioPlayer/tracks/trees.wav";
+import Ywen from "../../components/PageContent/AudioPlayer/tracks/ywen.wav";
 
 const MusicPage = (props) => {
   const { t, i18n } = useTranslation();
@@ -33,7 +42,7 @@ const MusicPage = (props) => {
           <section className="audio-container-parent">
             <div className="video-container-child">
               <VideoPlayer
-                url={t("listenHrefs.0")}
+                url={t("listenHrefs")}
                 width={"246px"}
                 height={"150px"}
                 // style={{
@@ -49,79 +58,67 @@ const MusicPage = (props) => {
           {/* Featured Item END HERE */}
 
           {/* List Section START HERE */}
-          <div className="temp-audio-container">
-            <AudioPlayer trackAudio={Dawn} trackTitle={"Dawn"} />
-          </div>
+
           <section>
             <ol className="audio-container-parent">
-              <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.1")}
-                  width={"246px"}
-                  height={"100px"}
+              <li className="audio-container-control">
+                <AudioPlayer
+                  trackAudio={Dawn}
+                  trackTitle={t("listenTrackTitle.0")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.2")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={CatchThisQuiet}
+                  trackTitle={t("listenTrackTitle.1")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.3")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={Dryw}
+                  trackTitle={t("listenTrackTitle.2")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.4")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={Gwenyn}
+                  trackTitle={t("listenTrackTitle.3")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.5")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={JustToBee}
+                  trackTitle={t("listenTrackTitle.4")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.6")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={Merfolk}
+                  trackTitle={t("listenTrackTitle.5")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.7")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={Petra}
+                  trackTitle={t("listenTrackTitle.6")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.8")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={Stars}
+                  trackTitle={t("listenTrackTitle.7")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.9")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={Trees}
+                  trackTitle={t("listenTrackTitle.8")}
                 />
               </li>
               <li className="video-container-child">
-                <VideoPlayer
-                  url={t("listenHrefs.10")}
-                  width={"246px"}
-                  height={"100px"}
+                <AudioPlayer
+                  trackAudio={Ywen}
+                  trackTitle={t("listenTrackTitle.9")}
                 />
               </li>
             </ol>
