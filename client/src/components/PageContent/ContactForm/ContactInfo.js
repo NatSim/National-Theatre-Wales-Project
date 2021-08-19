7814790aef156d19bd218dd7a2d9ab257796920d
NatSim <natsimbig@gmail.com>
2021-08-19 09:59:26 +0100

    adding br to contact page to keep text from over-flow

--- client/src/components/PageContent/ContactForm/ContactInfo.js
+++ client/src/components/PageContent/ContactForm/ContactInfo.js
@@ -8,19 +8,19 @@ const ContactInfo = () => {
     <div className="form-sizing-control">
       <ul>
         <li className="form-title-control">
-          {t("contactContent.2")}
+          {t("contactContent.2")} <br />
           <a className="anchor-control" href="info@nationaltheatrewales.org">
             info@nationaltheatrewales.org
           </a>
         </li>
         <li className="form-title-control">
-          {t("contactContent.3")}
+          {t("contactContent.3")} <br />
           <a className="anchor-control" href="+44 (0)29 2037 1689">
             +44 (0)29 2037 1689
           </a>
         </li>
         <li className="form-title-control">
-          {t("contactContent.4")}
+          {t("contactContent.4")} <br />
           <a className="anchor-control" href="+44 (0)29 2035 3070">
             +44 (0)29 2035 3070
           </a>
