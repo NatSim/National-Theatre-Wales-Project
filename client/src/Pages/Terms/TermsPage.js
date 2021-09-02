9011d21cf55fb6004ec3cabc10e6d122137c4d27
NatSim <natsimbig@gmail.com>
2021-09-02 21:30:06 +0100

    test: less text on terms page

--- client/src/Pages/Terms/TermsPage.js
+++ client/src/Pages/Terms/TermsPage.js
@@ -24,7 +24,19 @@ const Terms = () => {
 
         <h1 className="subtitle1">{t("termsContent.0")}</h1>
         <section>
-          <TermsContent />
+          {/* <TermsContent /> */}
+          <div>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
+            mollis quam at massa pulvinar, viverra tincidunt enim auctor.
+            Pellentesque nec leo nunc. Proin bibendum, dui eget dapibus
+            vestibulum, leo nibh interdum nulla, nec suscipit ante tellus vel
+            urna. Praesent tempor lacus vitae orci bibendum mattis. Mauris eu
+            maximus risus. Morbi nunc lectus, interdum nec eleifend ut,
+            elementum vitae quam. Suspendisse potenti. Cras id volutpat ante.
+            Integer et pellentesque risus, non congue turpis. Fusce et orci vel
+            tellus venenatis malesuada quis ac orci. Nunc suscipit enim in orci
+            placerat dignissim.
+          </div>
         </section>
       </div>
     </>
